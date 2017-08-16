8f248ced7214d4bbf309757ca3d9caa414702af7
Bertys <bertysitu@gmail.com>
2017-08-16 15:07:43 +0200

    Add files via upload
    Corrección de estructura de datos

---
+++ www/js/infoE1.js
@@ -0,0 +1,42 @@
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+//        deviceReadyM();
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+//        this.receivedEvent('deviceready');
+        deviceReadyM();
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+//////////////////START////////////////////////////
+function deviceReadyM() {
+    $("#infoE1").removeClass("hidden");
+    
+    $('#goBack').click(function(e) {
+    location.replace('./envios.html');
+        });
+
+}
